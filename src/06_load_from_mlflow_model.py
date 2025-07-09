0359a1a4a819cb3003d2d08cd297c145c7cd3f31
thomasheadrapson <thomasheadrapson@gmail.com>
2025-07-09 16:49:55 +0000

    section 5 part completed

--- src/06_load_from_mlflow_model.py
+++ src/06_load_from_mlflow_model.py
@@ -5,13 +5,13 @@ import pandas as pd
 # 1. Loading data
 # TODO: Replace with the path to your dataset
 print("Loading data...")
-data = pd.read_csv(None)
+data = pd.read_csv("data/fake_data.csv")
 X = data.drop(columns=["date", "demand"])
 X = X.astype('float')
 
 # 2. Define the path to the MLflow model
 # TODO: Replace with the path to your "rf_apples" folder created previously
-model_path = ''  # For example: '/home/ubuntu/MLflow/mlruns/EXPERIMENT_ID/RUN_ID/artifacts/rf_apples'
+model_path = '/home/ubuntu/MLflow_Course/mlruns/864063752250630287/4bdc560e8b47457c806ad22d3cdcc694/artifacts/rf_apples'  # For example: '/home/ubuntu/MLflow/mlruns/EXPERIMENT_ID/RUN_ID/artifacts/rf_apples'
 
 # 3. Load the model
 print("Loading model...")
@@ -23,7 +23,7 @@ predictions = model.predict(X)
 
 # 5. Calculate and display the average of predictions
 # TODO: Calculate the mean of predictions
-mean_prediction = 0  # Use the appropriate numpy or pandas function
+mean_prediction = predictions.mean()  # Use the appropriate numpy or pandas function
 
 print(f"\nResults:")
 print(f"Number of predictions: {len(predictions)}")
